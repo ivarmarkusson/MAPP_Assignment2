72de60dafa5a32d133729e5be6ab6104e7cc205d
ivarmarkusson <ivarmar15@ru.is>
2018-10-02 19:50:36 +0000

    delete functionality without correct index

--- assignment2/screens/HomeScreen.js
+++ assignment2/screens/HomeScreen.js
@@ -12,12 +12,6 @@ import Swipeable from 'react-native-swipeable';
 
 const data = require('../data/ass2data.json');
 
-const rightButtons = [
-  <TouchableOpacity>
-    <Text>Delete</Text>
-  </TouchableOpacity>,
-];
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -95,12 +89,40 @@ export default class HomeScreen extends React.Component {
     const lastName = item.contact.name.last_name;
     const fullName = `${firstName} ${lastName}`;
     return (
-      <Swipeable rightButtons={rightButtons}>
-        <TouchableOpacity onPress={() => this.onPress(item)}>
+      <Swipeable rightButtons={this.deleteButton(item.contact)}>
+        <TouchableOpacity onPress={() => this.onPress(item.contact)}>
           <Text style={styles.text}>{fullName}</Text>
         </TouchableOpacity>
       </Swipeable>
     );
+  };
+
+  deleteButton = contact => {
+    console.log('contact: ', contact);
+    console.log('contacts: ', this.state.contacts);
+    const index = _.findIndex(this.state.contacts, {
+      data: {
+        contact: {
+          name: {
+            first_name: contact.name.first_name,
+            last_name: contact.name.last_name,
+          },
+        },
+      },
+    });
+    console.log(index);
+    return [
+      <TouchableOpacity
+        style={{ backgroundColor: 'red' }}
+        onPress={() => {
+          this.setState({
+            contacts: this.state.contacts.splice(index, 1),
+          });
+        }}
+      >
+        <Text>Delete</Text>
+      </TouchableOpacity>,
+    ];
   };
 
   renderSectionHeader = ({ section: { title } }) => (
